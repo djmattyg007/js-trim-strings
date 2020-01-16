38d6886d07f6c512e7f1c88c5c2c3c99f9215ca9
Matthew Gamble <git@matthewgamble.net>
2020-01-16 20:46:24 +1100

    Add readme, remove unnecessary function left over from lodash port

---
+++ README.rst
@@ -0,0 +1,26 @@
+=======
+strtrim
+=======
+
+This package is simply a standalone version of lodash's trim functions.
+Credit goes to them for the implementation.
+
+Usage
+=====
+
+Usage is simple::
+
+    const { trim, trimStart, trimEnd } = require("strtrim");
+
+    console.log(trim(" abc ")); // outputs "abc"
+    console.log(trimStart(" abc ")); // outputs "abc "
+    console.log(trimEnd(" abc ")); // outputs " abc"
+
+    console.log(trim("-a b c-", "-")); // outputs "a b c"
+    console.log(trimStart("-_-abc_-_", "-_")) // outputs "abc_-_"
+    console.log(trimEnd("--abc__", "--")) // outputs "--abc__"
+
+Tests
+=====
+
+To run the test suite, run ``yarn run test`` in the root of this repository.
--- src/_internal/charsIndex.ts
+++ src/_internal/charsIndex.ts
@@ -1,27 +1,16 @@
-function strictIndexOf(chrSymbols: string[], chr: string): number {
-    let index = -1;
-    const { length } = chrSymbols;
-
-    while (++index < length) {
-        if (chrSymbols[index] === chr) {
-            return index;
-        }
-    }
-
-    return -1;
-}
-
 export function charsStartIndex(strSymbols: string[], chrSymbols: string[]): number {
     let index = -1;
     const length = strSymbols.length;
 
-    while (++index < length && strictIndexOf(chrSymbols, strSymbols[index]) > -1) {}
+    while (++index < length && chrSymbols.includes(strSymbols[index])) {}
+
     return index;
 }
 
 export function charsEndIndex(strSymbols: string[], chrSymbols: string[]): number {
     let index = strSymbols.length;
 
-    while (index-- && strictIndexOf(chrSymbols, strSymbols[index]) > -1) {}
+    while (index-- && chrSymbols.includes(strSymbols[index])) {}
+
     return index;
 }
